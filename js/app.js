915eb0fe6268dbf435a7d1f13a33dc49225b6bc4
sumitbishnoi00 <sumitbishnoi369@gmail.com>
2026-04-30 16:39:18 +0530

    advance to-do list

--- js/app.js
+++ js/app.js
@@ -1,31 +1,91 @@
 
 let input = document.getElementById("inputTask");
-let task = [];
 
+let editIndex = -1; 
 
 function enterBtn() {
-    let value = input.value.trim();
-        if(input.value === ""){
-            alert("Please enter something!");
-            return;
-        }
-        
+
+    let value = input.value.trim()
+
+    if (value === "") {
+        alert("Enter Something!");
+        input.value = ""
+        return;
+    }
+
+    let task = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    if (editIndex === -1) {
         task.push(value);
-        showTask();
+    } else {
+        task[editIndex] = value;
+        editIndex = -1;
+    }
 
-        input.value = "";
 
+    localStorage.setItem("tasks", JSON.stringify(task));
+
+    showTasks();
+    input.value = "";
 }
 
-    function showTask(){
-        let list = document.getElementById("listArr");
-        list.innerHTML = "";
-        
-     task.map ((task, index) => {  
-        let li = document.createElement("li");
-        li.innerText = task;
+function showTasks() {
 
-    list.appendChild(li);
+    let list = document.getElementById("listArr");
+    let task = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    list.innerHTML = "";
+
+    task.map((item, index) => {
+
+        let li = document.createElement("li");
+        li.innerHTML = `
+            <span onclick="editTask(${index})">${item}</span>
+            <span class="remove-btn" onclick="removeTask(${index})">❌</span>
+            `;
+        list.appendChild(li)
 
     });
 }
+
+function editTask(item){
+    
+    let task = JSON.parse(localStorage.getItem("tasks")) || [];
+    input.value = task[item];
+    editIndex = item;
+}
+
+function removeTask(item) {
+
+    let task = JSON.parse(localStorage.getItem("tasks")) || []
+
+    task.splice(item, 1);
+
+    localStorage.setItem("tasks", JSON.stringify(task));
+
+    showTasks();
+}
+
+window.onload = function () {
+    showTasks()
+};
+
+function popBtn() {
+    let task = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    task.pop();
+
+    localStorage.setItem("tasks", JSON.stringify(task));
+
+    showTasks();
+}
+
+function deleteBtn() {
+
+    localStorage.removeItem("tasks");
+
+    showTasks();
+
+}
+
+
